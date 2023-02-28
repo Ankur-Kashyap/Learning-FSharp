d5c7d46a88f04ced5e45fa2521d08b2ebd9f59d3
Ankur-Kashyap <mailtoankur.kashyap@gmail.com>
2023-02-28 11:23:05 +0530

    Ch07 Added

---
+++ Learning-FSharp/Ch07-LambdaExpressions/Program.fs
@@ -0,0 +1,63 @@
+﻿// A function to multiply input by 2
+let multBy2 x = x * 2
+
+// Alternate to multBy2, returns a lambda
+let multBy2Lambda = fun x -> x * 2
+
+// Applies function f over parameter x
+let apply1 f x = f x
+
+// A function to multiply 2 numbers
+let mult2Nums x y = x * y
+
+// Alternate to mult2NumsLambda, returns a lambda
+let mult2NumsLambda = fun x y -> x * y
+
+// Applies function f over parameter x and y
+let apply2 f x y = f x y
+
+[<EntryPoint>]
+let main args =
+
+    // next 5 statements produce the same result
+
+    // regular call to a function
+    multBy2 10 |> printf "%d"
+
+    // calling the lambda returned by multBy2Lambda
+    multBy2Lambda 10 |> printf "%d"
+
+    // in-place function definition, then call
+    (fun x -> x * 2) 10 |> printf "%d"
+
+    // calling apply1 for multBy2
+    apply1 multBy2 10 |> printf "%d"
+
+    // calling apply1 for multBy2Lambda
+    apply1 multBy2Lambda 10 |> printf "%d"
+
+    // calling apply1 for in-place function definition
+    apply1 (fun x -> x * 2) 10 |> printf "%d"
+
+
+    // next 5 statements produce the same result
+
+    // regular call to a function
+    mult2Nums 10 20 |> printf "%d"
+
+    // calling the lambda returned by mult2NumsLambda
+    mult2NumsLambda 10 20 |> printf "%d"
+
+    // in-place function definition, then call
+    (fun x y -> x * y) 10 20 |> printf "%d"
+
+    // calling apply2 for mult2Nums
+    apply2 mult2Nums 10 20 |> printf "%d"
+
+    // calling apply2 for mult2NumsLambda
+    apply2 mult2NumsLambda 10 20 |> printf "%d"
+
+    // calling apply2 for in-place function definition
+    apply2 (fun x y -> x * y) 10 20 |> printf "%d"
+
+    0
